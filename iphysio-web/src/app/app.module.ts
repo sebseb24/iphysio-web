36427268c7fbf75c63300233b288a25db0e0fa75
Billy <billy.mailhot@usherbrooke.ca>
2020-03-13 15:06:58 -0400

    integration du code frontend

--- iphysio-web/src/app/app.module.ts
+++ iphysio-web/src/app/app.module.ts
@@ -6,11 +6,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PatientComponent } from './patient/patient.component';
+import { PatientsComponent } from './patients/patients.component';
+//import { MockPatientComponent } from './mock-patient/mock-patient.component';
+import { PatientDetailComponent } from './patient-detail/patient-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PatientComponent
+    PatientComponent,
+    PatientsComponent,
+   // MockPatientComponent,
+    PatientDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ iphysio-web/src/app/patients/patients.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Patient } from '../patient';
+import { PATIENTS } from '../mock-patient/mock-patient.component';
+
+@Component({
+  selector: 'app-patients',
+  templateUrl: './patients.component.html',
+  styleUrls: ['./patients.component.scss']
+})
+export class PatientsComponent implements OnInit {
+
+
+  patients = PATIENTS;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  selectedPatient: Patient;
+
+  onSelect(patient: Patient): void {
+    this.selectedPatient = patient;
+  }
+
+}
