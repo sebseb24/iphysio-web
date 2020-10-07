0e26acc2ecb6676f8c209e0c58edd7056c973a3d
ayouda1986 <ayouda.1986@hotmail.fr>
2020-10-07 12:37:31 -0400

    Integration de la partie Chat dans notre code en utilisant la FireBase

--- iphysio-web/src/app/app.module.ts
+++ iphysio-web/src/app/app.module.ts
@@ -28,7 +28,20 @@ import { EditPatientComponent } from './edit-patient/edit-patient.component';
 import { PatientArchiveComponent } from './patient-archive/patient-archive.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
-
+import { ConnectComponent } from './connect/connect.component';
+import { RoomlistComponent } from './roomlist/roomlist.component';
+import { AddroomComponent } from './addroom/addroom.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { DatePipe } from '@angular/common';
 
 
 @NgModule({
@@ -48,12 +61,25 @@ import { PieChartComponent } from './pie-chart/pie-chart.component';
    LineChartComponent,
    PieChartComponent,
    routingComponents,
+   ConnectComponent,
+   RoomlistComponent,
+   AddroomComponent,
+   ChatroomComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatSnackBarModule,
+    MatSidenavModule,
     HttpClientModule,
     AutocompleteLibModule,
     RouterModule.forRoot([
@@ -77,7 +103,7 @@ import { PieChartComponent } from './pie-chart/pie-chart.component';
     DemoMaterialModule,
     MatNativeDateModule
   ],
-  providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } }, AuthGuard, { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true }],
+  providers: [DatePipe, { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } }, AuthGuard, { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true }],
   bootstrap: [AppComponent],
 
   entryComponents: [NouveauPatientComponent]
